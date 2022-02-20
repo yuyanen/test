d518d3fb03067d7f0295da4e17f490bd62f8a51e
yuyanen <jamesyuyanen@gmail.com>
2022-02-20 16:38:32 +0800

    ghj

--- C++/helloword/Project1/Project1.cpp
+++ C++/helloword/Project1/Project1.cpp
@@ -6,8 +6,8 @@
 int main()
 {   
     std::cout << "Hello World!\n";
-    std::cout << "welcome to USA!\n";
-    std::cout << "welcome to china!\n";
+    std::cout << "welcome to Singapore!\n";
+    std::cout << "happy new year!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
