d1887fe6d6a736e167e179b2ff713521fe989164
yuyanen <jamesyuyanen@gmail.com>
2022-02-20 15:09:01 +0800

    third edit

--- C++/helloword/Project1/Project1.cpp
+++ C++/helloword/Project1/Project1.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{   
+    std::cout << "Hello World!sfdasdfsdwerqwerqwersdrwer\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
